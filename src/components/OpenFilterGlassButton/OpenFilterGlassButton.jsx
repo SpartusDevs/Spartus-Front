38b71719fe328e4df7465273ad00cfbe22ac196d
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-11-15 16:59:27 -0300

    Responsible

--- src/components/OpenFilterGlassButton/OpenFilterGlassButton.jsx
+++ src/components/OpenFilterGlassButton/OpenFilterGlassButton.jsx
@@ -1,20 +1,26 @@
-import './OpenFilterGlassButton.css';
-import { SearchOutlined, FileDoneOutlined, ArrowLeftOutlined } from '@ant-design/icons'; // Importar el ícono de búsqueda
+import {
+  SearchOutlined,
+  FileDoneOutlined,
+  ArrowLeftOutlined,
+} from "@ant-design/icons"; 
 import { Link } from "react-router-dom";
-function OpenFilterGlassButton({ onClose}) {
-/* */
-    return (
-        <div style={{position:'absolute'}}>
-               <div className="glass-button_openFilter" >
-               <Link to="/"> <ArrowLeftOutlined className='icon_openFilter' />
-               </Link></div>
-    <div className="glass-button_openFilter" >
-    <SearchOutlined className='icon_openFilter' onClick={onClose}/>
-   
-    </div>
-     <div className="glass-button_openFilter" >
-     <FileDoneOutlined className='icon_openFilter'/>
-     </div>
+import "./OpenFilterGlassButton.css";
+function OpenFilterGlassButton({ onClose }) {
+  /* */
+  return (
+    <div className="container_openFilter" >
+      <div className="glass-button_openFilter">
+        <Link to="/">
+          {" "}
+          <ArrowLeftOutlined className="icon_openFilter" />
+        </Link>
+      </div>
+      <div className="glass-button_openFilter">
+        <SearchOutlined className="icon_openFilter" onClick={onClose} />
+      </div>
+      <div className="glass-button_openFilter">
+        <FileDoneOutlined className="icon_openFilter" />
+      </div>
     </div>
   );
 }
