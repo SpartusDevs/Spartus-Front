099acd5b8ac38244588503859a02b7830b6725ab
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-11-22 12:44:06 -0300

    FALTA HACERLO RESPONSIBLE

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import "./MenuBar.css"
 function MenuBar() {
   return (
     <div className="menu">
+    <p className='nav-item selected'>Inicio</p>
     <p className='nav-item'>Impacto Social</p>
     <p  className='nav-item'>Empresas</p>
     <p  className='nav-item'>Talento destacado</p>
+    <p className='nav-item selected'>ES</p>
   </div>
   )
 }
